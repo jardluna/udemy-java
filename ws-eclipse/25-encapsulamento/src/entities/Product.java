9e308158c7a4197c0ec3ea9eb5252ac291d3b55a
jardluna <134236491+jardluna@users.noreply.github.com>
2024-04-22 14:21:13 -0300

    Atualização

--- ws-eclipse/25-encapsulamento/src/entities/Product.java
+++ ws-eclipse/25-encapsulamento/src/entities/Product.java
@@ -19,29 +19,28 @@ public class Product {
 	public Product(String name, double price) {
 		this.name = name;
 		this.price = price;
-		this.quantity = 0;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public double getPrice() {
 		return price;
 	}
-	
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	
+
 	public int getQuantity() {
 		return quantity;
 	}
-	
+
 	public double totalValueInStock() {
 		return price * quantity;
 	}
