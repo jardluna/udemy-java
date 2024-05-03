85ff879415c5f167c25c42571822a4d8441ab7af
jardluna <134236491+jardluna@users.noreply.github.com>
2024-05-03 20:15:36 -0300

    Atualização

---
+++ ws-eclipse-exercicios/43-exercicio-poo/src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("How many employee will be registered?");
+		
+		int[] vect = new int[1];
+	}
+
+}
