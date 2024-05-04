a6caefe5b34e7a19c2a1db64fc53a5a0dc91ed18
jardluna <134236491+jardluna@users.noreply.github.com>
2024-05-04 15:08:47 -0300

    Atualizaão

--- ws-eclipse-exercicios/43-exercicio-poo/src/application/Program.java
+++ ws-eclipse-exercicios/43-exercicio-poo/src/application/Program.java
@@ -1,17 +1,58 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+import entities.Employee;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		List<Employee> list = new ArrayList<>();
+		Employee emp = new Employee();
 		
 		System.out.print("How many employee will be registered?");
+		int n = sc.nextInt();
+		System.out.println();
 		
-		int[] vect = new int[1];
+		for(int i = 1; i <= n; i++) {
+			System.out.printf("Employee #%d: %n", i);
+			System.out.print("Id: ");
+			Integer id = sc.nextInt();
+			System.out.print("Name: ");
+			String name = sc.next();
+			String sobrenome = sc.next();
+			System.out.print("Salary: ");
+			Double salary = sc.nextDouble();
+			list.add(new Employee(id, name, sobrenome, salary));
+			System.out.println();
+		}
+		
+		System.out.println("*Enter the employee Id that will ");
+		System.out.print("have salary increase: ");
+		Integer id = sc.nextInt();
+		emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		if(emp == null) {
+			System.out.println("*This is does not exist!");
+		}
+		else {
+			System.out.print("*Enter the percentage: ");
+			double percentage = sc.nextDouble();
+			emp.increaseSalary(percentage);
+		}
+		System.out.println();
+		
+		System.out.println("List of employee: ");
+		for(Employee obj : list) {
+			System.out.println(obj);
+		}
+		
+		sc.close();
 	}
-
+	
 }
+
+
--- ws-eclipse-exercicios/43-exercicio-poo/src/entities/Employee.java
+++ ws-eclipse-exercicios/43-exercicio-poo/src/entities/Employee.java
@@ -2,4 +2,64 @@ package entities;
 
 public class Employee {
 
+	//ATTRIBUTES
+	private Integer id;
+	private String name;
+	private String sobrenome;
+	private Double salary;
+	
+	//CONSTRUCTOR
+	public Employee() {
+		
+	}
+	
+	public Employee(Integer id, String name, String sobrenome, Double salary) {
+		this.id = id;
+		this.name = name;
+		this.sobrenome = sobrenome;
+		this.salary = salary;
+	}
+	
+	//GETTER AND SETTER
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Double getSalary() {
+		return salary;
+	}
+	
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	
+	//METHOD
+	public void increaseSalary(double percentage) {
+		salary = salary + (salary * percentage / 100);
+	}
+	
+	//FORMAT
+	public String toString() {
+		return id +
+				", " +
+				name.toUpperCase().charAt(0) + name.substring(1) +
+				" " +
+				sobrenome.toUpperCase().charAt(0) + sobrenome.substring(1) +
+				", " +
+				String.format("%.2f", salary);
+	}
 }
+
+
